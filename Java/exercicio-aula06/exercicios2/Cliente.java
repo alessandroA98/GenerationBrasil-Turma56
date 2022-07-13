e298b911f8f2d152826a199dae251e11205c1500
Alessandro Assuncao <alessandro.assuncao98@gmail.com>
2022-07-13 20:47:48 -0300

    adicionando exercicio de classes e metodos

---
+++ Java/exercicio-aula06/exercicios2/Cliente.java
@@ -0,0 +1,32 @@
+package br.com.aula06.exercicios2;
+
+public class Cliente {
+
+	private String nome;
+	private double peso;
+	private double tamanho;
+	
+	
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public double getPeso() {
+			return peso;
+		}
+		public void setPeso(double peso) {
+			this.peso = peso;
+		}
+		private double getTamanho() {
+			return tamanho;
+		}
+		public void setTamanho(double tamanho) {
+			this.tamanho = tamanho;
+		}
+	
+	
+	
+	
+}
