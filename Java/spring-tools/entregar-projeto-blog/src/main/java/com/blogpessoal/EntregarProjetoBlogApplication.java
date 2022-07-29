05e8f51dde04599d32433510cb2cad1a3c15aa75
Alessandro Assuncao <alessandro.assuncao98@gmail.com>
2022-07-29 16:38:38 -0300

    adicionando trabalho paraentrega de integacao spring ao banco

---
+++ Java/spring-tools/entregar-projeto-blog/src/main/java/com/blogpessoal/EntregarProjetoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregarProjetoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregarProjetoBlogApplication.class, args);
+	}
+
+}
